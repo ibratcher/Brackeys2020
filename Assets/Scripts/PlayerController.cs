9f8f74df3030b67f381f2c8458e0b3307ed522b8
Isaac Bratcher <isaac.j.bratcher@gmail.com>
2020-08-01 15:37:17 -0400

    Updates to jump movement and beginnings on camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpForce;
+    public float moveSpeed = 10;
+    public float jumpForce = 10;
 
     public CharacterController controller;
 
     private Vector3 moveDirection;
-    public float gravityScale = 1;
+    public float gravityScale = 1.5f;
 
     //public Rigidbody rb;
     // Start is called before the first frame update
@@ -30,13 +30,17 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }*/
 
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, 0);
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, moveDirection.z);
 
-        if(Input.GetButtonDown("Jump"))
+        if(controller.isGrounded)
         {
-            moveDirection.y = jumpForce;
+            if(Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W))
+            {
+                moveDirection.y = jumpForce;
+            }
         }
-        moveDirection.y = moveDirection.y + Physics.gravity.y * gravityScale;
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
     }
     
