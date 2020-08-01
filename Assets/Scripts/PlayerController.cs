2d869466ba692411afa3110edadf36ae8dc91220
Isaac Bratcher <isaac.j.bratcher@gmail.com>
2020-08-01 15:03:06 -0400

    Updated movement to use Character Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,38 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
-    public Rigidbody rb;
+
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale = 1;
+
+    //public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y, rb.velocity.z);
+        /*rb.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y, rb.velocity.z);
 
         if(Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }*/
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, 0);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
         }
+        moveDirection.y = moveDirection.y + Physics.gravity.y * gravityScale;
+        controller.Move(moveDirection * Time.deltaTime);
     }
+    
 }
