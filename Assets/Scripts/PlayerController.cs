844829411eb7c165b9d5c0c89f8bf168ed8af351
Isaac Bratcher <isaac.j.bratcher@gmail.com>
2020-08-01 14:03:45 -0400

    Updates to script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
