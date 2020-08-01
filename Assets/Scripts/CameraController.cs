44ad2a89fef4cc6c7b787f962922a3b2a8f3b7b3
Isaac Bratcher <isaac.j.bratcher@gmail.com>
2020-08-01 15:50:20 -0400

    Qualit y of life fixes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,20 @@ public class CameraController : MonoBehaviour
 {
 
     public Transform target;
+
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = target.position - transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        transform.position = target.position - offset;
+
         transform.LookAt(target);
     }
+    
 }
