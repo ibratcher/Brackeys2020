f44ebe34d0185c147c161881b6bec6128fe7fedb
Isaac Bratcher <isaac.j.bratcher@gmail.com>
2020-08-02 16:18:55 -0400

    Updates to door opening system

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    [SerializeField]
+    Transform target;
+
+    public float speed = 0.025f;
+
+    public bool isTriggered = false;
+    private Vector3 velocity = Vector3.zero;
+
+    void OnTriggerEnter(Collider col)
+    {
+        isTriggered = true;
+    }
+    
+    void Update()
+    {
+        if(isTriggered)
+        {
+            door.transform.position = Vector3.MoveTowards(door.transform.position, target.position, speed);
+        }
+    }
+}
